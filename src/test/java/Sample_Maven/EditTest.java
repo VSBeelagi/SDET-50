0c7eeca73521e387c56e2f7e212440029085d0ec
VSBeelagi <vijayalaxmibeelagi10@gmail.com>
2023-08-18 13:34:30 +0530

    ok

---
+++ src/test/java/Sample_Maven/EditTest.java
@@ -0,0 +1,12 @@
+package Sample_Maven;
+
+import org.testng.annotations.Test;
+
+public class EditTest {
+
+	@Test
+	public void editTest()
+	{
+		System.out.println("---- modified data --");
+	}
+}
